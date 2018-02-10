e333a168882900a3ccca229286cb567d34eb302d
Exert <billybernal@gmail.com>
2018-02-10 01:10:52 -0500

    Code Test

--- 2_Differences/src/diff_utils.c
+++ 2_Differences/src/diff_utils.c
@@ -1,13 +1,13 @@
 /*******************************************************************************
  * File: diff_utils.c
- * Author: 
+ * Author:
  * This file contains the implementation of two diff functions.
  * The first compares two lines
  * The second compares two files (and uses the first)
  ******************************************************************************/
 #include <stdio.h>
 #include "diff_utils.h"
- 
+
 void printUsage(char** argv){
   fprintf(stderr,"Usage: %s filename1 filename2\n",argv[0]);
   return;
@@ -16,7 +16,7 @@ void printUsage(char** argv){
 /*******************************************************************************
  * Function: diff_lines
  * Parameters: FILE* file1, FILE* file2
- * Reads a single line (until a newline) from each and compares each 
+ * Reads a single line (until a newline) from each and compares each
  * character.
  * Return value: The position of the first difference or -1 if there is a match
  ******************************************************************************/
@@ -29,7 +29,7 @@ int diff_lines(FILE *file1, FILE *file2){
  * Parameters: FILE* file1, FILE* file2
  * Reads a file a line at a time and uses diff_lines to compare the by
  * character.
- * Return value: The line position of the first difference or -1 if there is a 
+ * Return value: The line position of the first difference or -1 if there is a
  * match
  ******************************************************************************/
 int diff_files(FILE *file1, FILE *file2){
@@ -39,7 +39,7 @@ int diff_files(FILE *file1, FILE *file2){
 /*******************************************************************************
  * Function: diff_files_cmdline
  * Parameters: int argc, char** argv
- * Opens the files names in the command line arguments and diffs them until 
+ * Opens the files names in the command line arguments and diffs them until
  * the first failure. Prints the location of the first failure.
  * Return value: 0 if the files match, -1 if the files don't match, -2 if
  * there was an error
@@ -47,4 +47,3 @@ int diff_files(FILE *file1, FILE *file2){
 int diff_files_cmdline(int argc, char** argv){
   return 0;
 }
-
